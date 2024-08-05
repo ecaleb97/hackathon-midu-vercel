c834433b2d2c1ab4eab370884b5972c8307809fd
ecaleb97 <valley_rob.0c@icloud.com>
2024-08-06 00:22:18 +0200

    add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,22 @@
+export const items = [
+	{
+		title: "Vegetariano",
+		href: "/recetas/vegetariano",
+	},
+	{
+		title: "Vegano",
+		href: "/recetas/vegano",
+	},
+	{
+		title: "Ensaladas",
+		href: "/recetas/ensaladas",
+	},
+	{
+		title: "Postres",
+		href: "/recetas/postres",
+	},
+	{
+		title: "Carnes",
+		href: "/recetas/carnes",
+	},
+];
