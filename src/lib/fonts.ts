f2c67a45b49cb57741304471a852e61f684718e2
ecaleb97 <valley_rob.0c@icloud.com>
2024-08-06 00:20:52 +0200

    add fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,3 +1,7 @@
 import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
+import { Roboto } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+export const montserrat = Montserrat({ subsets: ["latin"] });
+export const roboto = Roboto({ subsets: ["latin"], weight: "900" });
