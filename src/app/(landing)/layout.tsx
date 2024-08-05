5ff51048bba57ddc954b0c7fab5f5aa3930e4ec9
ecaleb97 <valley_rob.0c@icloud.com>
2024-08-06 00:26:47 +0200

    add landing page

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,52 @@
+import { MobileSidebar } from "@/components/nav/mobile-sidebar";
+import { items } from "@/lib/constants";
+import type { ChildrenProps } from "@/lib/types";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { montserrat } from "@/lib/fonts";
+
+export default function LandingLayout({ children }: ChildrenProps) {
+	return (
+		<div className="min-h-screen max-w-full bg-slate-100">
+			<header className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
+				<div className={`flex gap-2 text-xl ${montserrat.className}`}>
+					<Image src="/logo-hackathon.svg" alt="Logo" width={50} height={50} />
+					Recetas.AI
+				</div>
+				<Navbar />
+				<ExplorerButton />
+				<MobileSidebar />
+			</header>
+			{children}
+		</div>
+	);
+}
+
+function Navbar() {
+	return (
+		<ul
+			className="hidden md:flex items-center gap-6 bg-[#FBFBFB] 
+			px-5 py-3 rounded-full shadow-sm border border-gray-200"
+		>
+			{items.map((item) => (
+				<li key={item.href} className="text-sm">
+					<Link
+						href={item.href}
+						className="text-gray-900/60 hover:text-gray-800"
+					>
+						{item.title}
+					</Link>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+function ExplorerButton() {
+	return (
+		<Button className="rounded-full hidden md:flex items-center" asChild>
+			<Link href={"/recipes"}>Explorar</Link>
+		</Button>
+	);
+}
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { montserrat } from "@/lib/fonts";
+import Link from "next/link";
+
+export default function LandingPage() {
+	return (
+		<main>
+			<LandingHero />
+		</main>
+	);
+}
+
+function LandingHero() {
+	return (
+		<div className="text-center space-y-6 py-20 px-4">
+			<h1
+				className={`text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl ${montserrat.className}`}
+			>
+				Descubre lo mejor de cada <br />{" "}
+				<span
+					className="bg-gradient-to-r from-amber-500 
+					via-orange-600 to-yellow-500 bg-clip-text text-transparent"
+				>
+					gastronomía
+				</span>
+			</h1>
+			<h2 className="text-gray-600 sm:text-xl">
+				Recetas.ai te ayuda a encontrar las mejores recetas de todo el mundo
+			</h2>
+			<Button className="rounded-full px-6" asChild>
+				<Link href={"/recipes"}>Explora gastronomías</Link>
+			</Button>
+		</div>
+	);
+}
