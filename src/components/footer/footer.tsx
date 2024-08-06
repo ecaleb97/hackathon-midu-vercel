fe85ea58de494df93c7e9d7715ec514f64f0665c
ecaleb97 <valley_rob.0c@icloud.com>
2024-08-06 14:41:39 +0200

    update footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export function Footer() {
 	return (
-		<footer className="text-gray-900/60 text-center text-sm p-2">
+		<footer className="text-gray-900/60 text-center text-sm p-2 mt-4">
 			Desarrollado por Caleb Villanueva Zamalloa -{" "}
-			<Button asChild variant={"link"} className="text-gray-900/60 p-0">
+			<Button asChild variant={"link"} className="text-gray-900/60 p-0 m-0">
 				<Link href={"https://ecaleb.dev"} target="_blank">
 					ecaleb.dev
 				</Link>
